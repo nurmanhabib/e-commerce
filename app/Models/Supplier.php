ba7a9145919adc76c9f01dc7652ee904dd5e9283
Radhian Prayogo <radhian21@gmail.com>
2016-02-22 18:31:28 +0700

    update model supplier

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,17 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 
-class Supplier extends Model implements
-    AuthenticatableContract,
-    AuthorizableContract
+class Supplier extends Model
 {
-    use Authenticatable, Authorizable;
 
     /**
      * The attributes that are mass assignable.
