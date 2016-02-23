0e9479273b85dcd2eca3a1a37d12b3dacfb36e4d
Radhian Prayogo <radhian21@gmail.com>
2016-02-23 19:21:03 +0700

    tambah lumen generator

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -82,7 +82,7 @@ $app->singleton(
 |
 */
 
-//$app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
 $app->register(App\Providers\ValidatorServiceProvider::class);
