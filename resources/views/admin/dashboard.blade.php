61836bd38a10cb57381d2ab45217e9d541aa2510
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2016-02-23 22:28:28 +0700

    Perbarui template for admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Control Panel - Amtek Ecommerce v1.0.0'</h1>
+    <h1>Control Panel - Amtek Ecommerce v1.0.0</h1>
 @stop
--- resources/views/front/homepage.blade.php
+++ resources/views/front/homepage.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.front')
 
 @section('content')
-    <h1>Amtek Ecommerce v1.0.0'</h1>
+    <h1>Amtek Ecommerce v1.0.0</h1>
 @stop
