0b20ba4117c9e797744659b5b2499a5423b25865
Singgih Saputro <singgihsap@gmail.com>
2016-02-25 10:21:03 +0700

    authentication dengan vue js

---
+++ public/vue/http.js
@@ -0,0 +1,15 @@
+// URL and endpoint constants
+const API_URL = 'http://192.168.1.15:8000/api/v1'
+const SITE_URL = 'http://192.168.1.15:8000'
+
+// API Endpoint
+const LOGIN_URL = API_URL + '/auth/login'
+const SIGNUP_URL = API_URL + '/register'
+
+// Link SITE
+const LOGIN_USER = SITE_URL + '/login'
+const LOGIN_ADMIN = SITE_URL + '/admin'
+const LOGIN_VENDOR = SITE_URL + '/vendor'
+const FORGOT_PASSWORD = SITE_URL + '/forgot-password'
+
+const ADMIN_SITE = SITE_URL + '/admin/dashboard'
