f5bc7c61d03f6cc141904a300b618be21a18d4aa
Singgih Saputro <singgihsap@gmail.com>
2016-02-25 10:17:46 +0700

    update layouts login

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.login')
+
+@section('content')
+	<form>
+		<div class="panel panel-body login-form">
+			<div class="text-center">
+				<div class="icon-object border-warning text-warning"><i class="icon-spinner11"></i></div>
+				<h5 class="content-group">Password recovery <small class="display-block">We'll send you instructions in email</small></h5>
+			</div>
+
+			<div class="form-group has-feedback">
+				<input type="email" class="form-control" placeholder="Your email">
+				<div class="form-control-feedback">
+					<i class="icon-mail5 text-muted"></i>
+				</div>
+			</div>
+
+			<button type="submit" class="btn bg-blue btn-block">Reset password <i class="icon-arrow-right14 position-right"></i></button>
+		</div>
+	</form>
+@stop
+
+@section('script')
+@stop
