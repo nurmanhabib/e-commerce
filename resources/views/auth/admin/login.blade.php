0937b2b415812f3ce681c33f1fda8eb388710b21
Radhian Prayogo <radhian21@gmail.com>
2016-02-26 22:05:58 +0700

    update di view login admin

--- resources/views/auth/admin/login.blade.php
+++ resources/views/auth/admin/login.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.login')
 
 @section('content')
-	<form id="login">
+	<form id="login" v-on:submit.prevent="loginAdmin">
 		<div class="panel panel-body login-form">
 			<div class="text-center">
 				<div class="icon-object border-slate-300 text-slate-300"><i class="icon-reading"></i></div>
 				<h5 class="content-group">Login to your account <small class="display-block">Your credentials</small></h5>
 			</div>
+
+			@{{error}}
 
 			<div class="form-group has-feedback has-feedback-left">
 				<input v-model="credentials.email" type="email" class="form-control" placeholder="Email">
@@ -16,7 +18,7 @@
 			</div>
 
 			<div class="form-group has-feedback has-feedback-left">
-				<input v-model="credentials.password" type="text" class="form-control" placeholder="Password">
+				<input v-model="credentials.password" type="password" class="form-control" placeholder="Password">
 				<div class="form-control-feedback">
 					<i class="icon-lock2 text-muted"></i>
 				</div>
@@ -38,7 +40,7 @@
 			</div>
 
 			<div class="form-group">
-				<button v-on:click="loginAdmin" type="button" class="btn bg-blue btn-block">Login <i class="icon-arrow-right14 position-right"></i></button>
+				<button type="submit" class="btn bg-blue btn-block">Login <i class="icon-arrow-right14 position-right"></i></button>
 			</div>		
 		</div>
 	</form>
