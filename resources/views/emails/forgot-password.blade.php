600a801c476565ccf78b411d2f8770dded017569
Radhian Prayogo <radhian21@gmail.com>
2016-02-26 16:40:56 +0700

    tambah template untuk forgot password

---
+++ resources/views/emails/forgot-password.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.email')
+
+@section('content')
+    <div id=":253" class="ii gt m1531b755ebbeb0f6 adP adO">
+		<div id=":24x" class="a3s" style="overflow: hidden;">
+			<u></u>
+			<div style="background:#f9f9f9;color:#373737;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;font-size:17px;line-height:24px;max-width:100%;width:100%!important;margin:0 auto;padding:0">
+				<table style="border-collapse:collapse;line-height:24px;margin:0;padding:0;width:100%;font-size:17px;color:#373737;background:#8BB9F5; border-radius: 10px;" border="0" cellpadding="0" cellspacing="0" width="100%">
+					<tbody>
+						<tr>
+							<td style="border-collapse:collapse" valign="top">
+								<table style="border-collapse:collapse" border="0" cellpadding="0" cellspacing="0" width="100%">
+									<tbody>
+										<tr>
+											<td style="border-collapse:collapse;padding:20px 16px 12px" valign="bottom">
+												<div style="text-align:center">
+													<a href="https://www.slack.com" style="color:#439fe0;font-weight:bold;text-decoration:none;word-break:break-word" target="_blank">
+																				<!-- <img class="CToWUd" src="https://ci5.googleusercontent.com/proxy/79C4QYCVhMwY_KK8LVXiGp6kz_c6D71WL_zVp6r3IOo0TLVxYanwjlq0eoG2c4MFV0s9GOU3VA1PX5CotkszUbTw_cL4CqyhoUnNRhqLyQDD_9NVlg=s0-d-e1-ft#https://slack.global.ssl.fastly.net/66f9/img/slack_logo_240.png" style="outline:none;text-decoration:none;border:none;width:120px;min-height:36px"> -->
+																				<h2>LOGO</h2>
+													</a>
+												</div>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+						</tr>
+						<tr>
+							<td style="border-collapse:collapse" valign="top">	
+								<table style="border-collapse:collapse;background:white;border-radius:0.5rem;margin-bottom:1rem" align="center" border="0" cellpadding="32" cellspacing="0">
+									<tbody>
+										<tr>
+											<td style="border-collapse:collapse" valign="top" width="556">
+												<div style="max-width:540px;margin:0 auto">
+													<div style="background:white;border-radius:0.5rem;margin-bottom:1rem">
+														<h2 style="color:#2ab27b;line-height:30px;margin-bottom:12px;margin:0 0 12px">
+															<center>Hai {{ $email }} !!</center>
+														</h2>
+
+														<p style="font-size:18px;line-height:24px;margin:0 0 16px">
+															Please click link bellow to reset your password!
+														</p>
+
+														<p>
+															Your link reset password: 
+															<a href="{{ $link }}">Click here</a>
+														</p>
+
+													</div>
+												</div>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+						</tr>
+						<tr>
+							<td style="border-collapse:collapse">
+								<table style="border-collapse:collapse;margin-top:1rem;background:white;color:#989ea6" align="center" border="0" cellpadding="0" cellspacing="0" width="100%">
+									<tbody>
+										<tr>
+											<td style="border-collapse:collapse;height:5px;background-image:url('https://ci4.googleusercontent.com/proxy/HzVdB1b8ViwO5j9_pLX5Fk4HKXY232E1BpGTE7cX4LYydeKxvOnovyQv85NrUdewb44TDk3Ydo0VEPG9UAtF351uBDMZu1Hcv9bgR_qho9VfyNAelMdW=s0-d-e1-ft#https://slack.global.ssl.fastly.net/66f9/img/email-ribbon_@2x.png');background-repeat:repeat-x">
+											</td>
+										</tr>
+										<tr>
+											<td style="border-collapse:collapse;padding:16px 8px 24px" align="center" valign="top">
+												<div style="max-width:600px;margin:0 auto">
+													<a href="#">admin@amtekcommerce.com</a>
+												</div>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="yj6qo"></div><div class="adL">
+			</div>
+		</div>
+	</div>
+@stop
