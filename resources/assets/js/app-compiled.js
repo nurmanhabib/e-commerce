4d85b227e7e62761002c06b62eb7762577bba8a2
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2016-02-29 15:45:48 +0700

    try with elixir browserify

---
+++ resources/assets/js/app-compiled.js
@@ -0,0 +1,9 @@
+import Loading from './components/Loading.vue';
+
+export default {
+    components: {
+        'loading': Loading
+    }
+};
+
+//# sourceMappingURL=app-compiled.js.map
---
+++ resources/assets/js/app.js
@@ -0,0 +1,7 @@
+import Loading from './components/Loading.vue';
+
+export default {
+    components: {
+        'loading': Loading
+    }
+}
