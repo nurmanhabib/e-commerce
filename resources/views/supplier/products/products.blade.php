7d9cd7f72dc51aef5df61fab72db44d5f8574d57
Radhian Prayogo <radhian21@gmail.com>
2016-03-02 09:59:26 +0700

    update view crud products

--- resources/views/supplier/products/products.blade.php
+++ resources/views/supplier/products/products.blade.php
@@ -105,6 +105,13 @@
 									</div>
 								</div>
 
+								<div class="form-group">
+									<label class="control-label col-lg-4">Tags</label>
+									<div class="col-lg-8">
+										<input v-model="newProducts.tags" type="text" class="form-control">
+									</div>
+								</div>
+
 							</fieldset>
 						</form>
 					</div>
