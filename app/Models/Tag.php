64941213ee2ab52fad87f5dee5b46e71855dd64d
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2016-03-11 22:40:48 +0700

    generate slug

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,6 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+	public $timestamps = false;
+	protected $fillable = ['name'];
+
+	public static function boot()
+	{
+		parent::boot();
+
+		Tag::creating(function (Tag $tag) {
+			$tag->generateSlug();
+		});
+	}
+
+	public function generateSlug()
+	{
+		$this->slug = $this->createSlug($this->name);
+
+		return $this;
+	}
+
+	public function createSlug($name)
+	{
+		return str_slug($name);
+	}
+
 	public function products()
 	{
 		return $this->morphedByMany(Product::class, 'taggable');
