6ff02622b565bc2fbd2d0861f60d816b8016895e
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2016-03-11 22:44:51 +0700

    view for public

---
+++ resources/views/front/include/header.blade.php
@@ -0,0 +1,21 @@
+<!-- start:navbar-main -->
+<section id="navbar-main">
+    <nav class="navbar">
+        <div class="container">                 
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="header-top-left">
+                    <a href="#" class="navbar-brand">
+                        AmtekCommerce
+                    </a>
+                    <i class="navbar-text fa fa-bars"></i>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="header-top-right pull-right">
+                    <img src="{{ asset('assets/images/icon/chart.svg') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </nav>
+</section>
+<!-- end:/navbar-main -->
