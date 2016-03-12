6f5802bb8f74a7119e6f5b9fc009265ed1aeb77e
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2016-03-12 21:31:49 +0700

    default config amtekcommerce

--- config/amtekcommerce.php
+++ config/amtekcommerce.php
@@ -1,8 +1,66 @@
 <?php
 
 return [
-    'name'		=> 'AmtekCommer',
-    'slogan'	=> 'Online Marketplace',
-    'logo'		=> 'image/logo.jpg',
-    'code'		=> 'AMC',
+    'name'      => 'AmtekCommerce',
+    'slogan'    => 'Online Marketplace',
+    'logo'      => 'assets/images/logo_light.png',
+    'code'      => 'AMC',
+    'social'    => [
+        'facebook'  => [
+            'link'  => null,
+        ],
+        'instagram'  => [
+            'link'  => null,
+        ],
+    ],
+    
+    'email'     => [
+        'from'  => [
+            'email' => 'no-reply@amtekcommerce.com',
+            'name'  => 'AmtekCommerce',
+        ],
+    ],
+
+    'register'  => [
+        'validation'    => [
+            'username'  => ['required', 'min:5', 'max:20'],
+            'password'  => ['required', 'min:8'],
+        ]
+    ],
+
+    'transaction'   => [
+        'payment'   => ['due' => 3 * 60], // dalam menit
+    ],
+
+    'socialauth'    => [
+        'facebook'  => [
+            'api'       => ['value' => null, 'autoload' => false],
+            'token'     => ['value' => null, 'autoload' => false],
+            'secret'    => ['value' => null, 'autoload' => false],
+        ],
+
+        'twitter'   => [
+            'api'       => ['value' => null, 'autoload' => false],
+            'token'     => ['value' => null, 'autoload' => false],
+            'secret'    => ['value' => null, 'autoload' => false],
+        ],
+
+        'google'    => [
+            'api'       => ['value' => null, 'autoload' => false],
+            'token'     => ['value' => null, 'autoload' => false],
+            'secret'    => ['value' => null, 'autoload' => false],
+        ],
+
+        'instagram' => [
+            'api'       => ['value' => null, 'autoload' => false],
+            'token'     => ['value' => null, 'autoload' => false],
+            'secret'    => ['value' => null, 'autoload' => false],
+        ],
+
+        'line'      => [
+            'api'       => ['value' => null, 'autoload' => false],
+            'token'     => ['value' => null, 'autoload' => false],
+            'secret'    => ['value' => null, 'autoload' => false],
+        ],
+    ],
 ];
